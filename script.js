d037e3f2a7042ada2ed7d36b41dc0add7dc6ac83
fuhr-br <58981492+fuhr-br@users.noreply.github.com>
2020-04-08 11:26:27 -0300

    Add files via upload
    Init Project

---
+++ script.js
@@ -0,0 +1,21 @@
+
+function onOff(){/*Adiciona a função de click no botão com a classe fat*/
+    document
+    .querySelector("#modal") /*seleciona a id footer*/
+    .classList
+    .toggle("hide")/*esta função liga ou desliga essa classe hide*/
+
+    document
+        .querySelector("body")
+        .classList
+        .toggle("hideScroll")
+
+        document
+        .querySelector("#modal")
+        .classList
+        .toggle("addScroll")
+}
+      
+   
+        
+   
